47adb099dad5966819073a7417f11a1e4a92a25c
AbhiJeetSoni22 <AbhiJeetSoni22>
2024-12-19 22:03:41 +0530

    github data added

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
-
+const obj = {
+    name: 'Abhi',
+    age: 20,
+    city: 'Shangarh'
+}
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/abhi', (req, res) => {
-  res.send('Hello abhi World!')
+app.get('/abhiinfo', (req, res) => {
+  res.json(obj)
 })
 
 app.listen(process.env.PORT, () => {
